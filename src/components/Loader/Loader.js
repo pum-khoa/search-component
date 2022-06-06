e4044483330e34c4aad6db16cca78167b5d13ae1
pumk <dangkhoa.duong@nfq.asia>
2022-06-06 14:32:30 +0700

    chore: remove console log

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Loader.css';
 const Loader = (props) => {
-  console.log(props);
   if (props.loading)
     return <div className="loader-wrapper">{props.children}</div>;
   else return <div>{props.children}</div>;
